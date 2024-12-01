07f522fd52db22573fb01058badea6b210e37bfa
daridev <darideveloper@gmail.com>
2024-12-01 13:43:07 -0600

    feat (code): update dashboard endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
-export const apiBaseUrl = 'https://ezbookingtours-store.herokuapp.com/driven-mastermind'
+export const apiBaseUrl = 'https://omar.apps.darideveloper.com/driven-mastermind'
 // export const apiBaseUrl = 'http://localhost:8000/driven-mastermind'
 export const saleEndpoint = `${apiBaseUrl}/sale/`
